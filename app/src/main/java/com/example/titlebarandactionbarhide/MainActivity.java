07fd1f30df0188fd839806cc7684e285fb1dc8bb
Asadu <techasadbd@gmail.com>
2023-09-14 13:34:57 +0600

    Initial commit

---
+++ app/src/main/java/com/example/titlebarandactionbarhide/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.titlebarandactionbarhide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Remove the title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // Remove the notification bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+    }
+}
